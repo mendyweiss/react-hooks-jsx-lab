e79bd290e9d15d9d36d860607d0be3f240eafe26
Mendy <mendyw94@gmail.com>
2022-08-31 14:39:13 -0400

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I'm a 28 year old junior developer looking for succsess and happieness for myself and the ones around me!</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
